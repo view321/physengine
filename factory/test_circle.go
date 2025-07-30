13deab9e6d3468953c18a8e1f684c78150e2041d
view321 <57069886+view321@users.noreply.github.com>
2025-07-30 13:56:08 +0300

    6

--- factory/test_circle.go
+++ factory/test_circle.go
@@ -10,22 +10,18 @@ import (
 )
 
 func CreateTestCircle(ecs *ecs.ECS, pos Vec2.Vec2, vel Vec2.Vec2) *donburi.Entry {
-	entity := ecs.World.Create(components.MaterialComponent, components.Transform, components.CircleCollider, components.Drawable, components.MassComponent, components.Velocity, components.AngularVelocity, components.Torque)
+	entity := ecs.World.Create(components.MaterialComponent, components.Transform, components.CircleCollider, components.Drawable, components.MassComponent, components.Velocity)
 	entry := ecs.World.Entry(entity)
 	components.SetPos(entry, pos)
 	components.Velocity.Get(entry).Velocity = vel
-	components.SetAngularVelocity(entry, -1.5) // Add some rotation in opposite direction
 	img, _ := assets.GetImage("D:/Coding/physengine/assets/assets/player.png")
 	components.Drawable.Get(entry).Sprite = img
 	components.Transform.Get(entry).Scale = Vec2.Vec2{X: 1, Y: 1}
 	mc := components.MassComponent.Get(entry)
 	mc.Mass = 10
 	mc.InverseMass = 1 / mc.Mass
-	// Calculate inertia for a circle
 	crcl := components.CircleCollider.Get(entry)
 	crcl.Radius = 100
-	mc.Inertia = mc.Mass * crcl.Radius * crcl.Radius * 0.5 // I = 0.5 * m * r^2 for a circle
-	mc.InverseInertia = 1 / mc.Inertia
 	mat := components.MaterialComponent.Get(entry)
 	mat.Restitution = 0.8
 	return entry
--- factory/test_square.go
+++ factory/test_square.go
@@ -10,22 +10,18 @@ import (
 )
 
 func CreateTestSquare(ecs *ecs.ECS, pos Vec2.Vec2, vel Vec2.Vec2) *donburi.Entry{
-	entity := ecs.World.Create(components.MaterialComponent, components.Transform, components.AABB_Component, components.Drawable, components.MassComponent, components.Velocity, components.AngularVelocity, components.Torque)
+	entity := ecs.World.Create(components.MaterialComponent, components.Transform, components.AABB_Component, components.Drawable, components.MassComponent, components.Velocity)
 	entry := ecs.World.Entry(entity)
 	components.SetPos(entry, pos)
 	components.Velocity.Get(entry).Velocity = vel
-	components.SetAngularVelocity(entry, 2.0) // Add some rotation
 	img, _ := assets.GetImage("D:/Coding/physengine/assets/assets/player.png")
 	components.Drawable.Get(entry).Sprite = img
 	components.Transform.Get(entry).Scale = Vec2.Vec2{X: 1, Y: 1}
 	mc := components.MassComponent.Get(entry)
 	mc.Mass = 10
 	mc.InverseMass = 1 / mc.Mass
-	// Calculate inertia for a square (approximate)
-	mc.Inertia = mc.Mass * 10000 // Simplified inertia calculation
-	mc.InverseInertia = 1 / mc.Inertia
 	box := components.AABB_Component.Get(entry)
-	box.Min = Vec2.Vec2{-100, -50}
+	box.Min = Vec2.Vec2{-100, -100}
 	box.Max = Vec2.Vec2{50, 50}
 	mat := components.MaterialComponent.Get(entry)
 	mat.Restitution = 0.8
